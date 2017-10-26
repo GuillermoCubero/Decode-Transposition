571883655e0824606c4a8e9040804fe262f8577c
QbP <QbP@138.4.222.69>
2017-10-26 04:08:02 +0200

    Arreglando problemas de ejecución

--- src/trans/DecodeTransposition.java
+++ src/trans/DecodeTransposition.java
@@ -3,9 +3,8 @@ package trans;
 public class DecodeTransposition {
     
     public static void main(String [] args){
-        InputOutput datos = new InputOutput();
-        datos.leerDatos();
-        Decodification resultado = new Decodification();
-        resultado.Input(datos);
+        InputOutput data = new InputOutput();
+        data.leerDatos();
+        data.mostrarDatos();
     }
 }
