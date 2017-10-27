8130cf51f985809e59c5b1b12675c58e1dacf34c
QbP <guille22ce@gmail.com>
2017-10-27 15:23:32 +0200

    Nuevo commit

--- src/trans/Decodification.java
+++ src/trans/Decodification.java
@@ -7,21 +7,30 @@ public class Decodification {
     protected Character[][] DecodeWithKey(String key, String code) {
         int symbols = code.length()/key.length();
         int overflow = code.length()%key.length();
-        Character solution[][] = new Character[key.length()][symbols+1];
+        int count = 0;
+        int rows = symbols;
+        if (overflow > 1)rows++;
+        Character solution[][] = new Character[key.length()][rows];
+        ArrayList<Integer> transformation = KeyTransformation(key);
         
-        
-        for (int i = 0; i < key.length(); i++) {
-            if(KeyTransformation(key).get(i) < key.length()-overflow){
-                for (int j = 0; j < symbols; j++) {
+        for (int i = 0 ; i < key.length(); i++) {
+            
+            
+            
+            
+            if(overflow > 1){
+                for (int j = 0; j < symbols+1; j++) {
                     solution[i][j] = code.charAt(symbols*KeyTransformation(key).get(i)+j);
+                    count++;
                 }
-                
             }else{
-                for (int j = 0; j < symbols +1; j++) {
-                    solution[i][j] = code.charAt(symbols*KeyTransformation(key).get(i)+j);
-                }
+                for (int j = 0; j < symbols; j++) {
+                solution[KeyTransformation(key).get(i)][j] = code.charAt(symbols*KeyTransformation(key).get(i)+j);
+            }
             }
         }
+            
+            
         return solution;
     }
 
@@ -42,12 +51,22 @@ public class Decodification {
         result = (ArrayList<Integer>) transformation.clone();
         
         for (int i = 0; i < key.length(); i++) {
-            int position = transformation.indexOf(Collections.max(transformation));
+            int position = MaxValue(transformation);
             transformation.set(position, 0);
             result.set(position, key.length() - (i+1));
         }
+        System.out.println(result);
         return result;
-        
+    }
+    
+    private int MaxValue(ArrayList<Integer> transformation){
+        int pointer = 0;
+        for (int i = transformation.size()-1; i > 0; i--) {
+            if (transformation.get(i) > transformation.get(pointer)) {
+                pointer = i;
+            }
+        }
+        return pointer;
     }
 
 }
